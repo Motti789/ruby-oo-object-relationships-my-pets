558b86bb2240c74e824eb08b8c4d6e4b211fc6ee
Motti789 <mchillywilly@gmail.com>
2021-07-28 16:56:56 -0400

    commit tree 7e73d1de5ed4f16bb4cab09faa225f6766fec999

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,8 +34,34 @@ class Owner
   end
 
   def buy_cat(name)
-    Cat.all.count {|cat| cat.owner == self}
+    Cat.new(name, self)
   end  
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end  
+
+  def walk_dogs
+    self.dogs.each { |dog| dog.mood = "happy" }
+  end
+
+  def feed_cats
+    self.cats.each { |cat| cat.mood = "happy" }
+  end  
+
+  def sell_pets
+    pets = self.dogs + self.cats
+    pets.each { |pet| pet.mood = "nervous" }
+    pets.each { |pet| pet.owner = nil }
+  end  
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end  
+
+
+
+
  
 
 
